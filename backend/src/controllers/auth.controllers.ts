e4c16e1c6df98650cc8e4feccda015dabbee3eb5
Subhayan Das <subhayan.sd10@gmail.com>
2024-02-04 17:38:11 +0530

    controllers added

---
+++ backend/src/controllers/auth.controllers.ts
@@ -0,0 +1,82 @@
+import Users from "../models/users.model";
+import bcryptjs from "bcryptjs";
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+const register = async (req:Request, res:Response) => {
+  try {
+    const { fullName, email, password } = req.body;
+
+    if (!fullName || !email || !password) {
+      res.status(400).send("Please fill all required fields");
+    } else {
+      const isAlreadyExist = await Users.findOne({ email });
+      if (isAlreadyExist) {
+        res.status(400).send("User already exists");
+      } else {
+        const newUser = new Users({ fullName, email });
+        bcryptjs.hash(password, 10, (err, hashedPassword) => {
+          newUser.set("password", hashedPassword);
+          newUser.save();
+        });
+        return res.status(200).send("User registered successfully");
+      }
+    }
+  } catch (error) {
+    console.log(error, "Error");
+  }
+};
+
+const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      res.status(400).send("Please fill all required fields");
+    } else {
+      const user = await Users.findOne({ email });
+      if (!user) {
+        res.status(400).send("User email or password is incorrect");
+      } else {
+        const validateUser = await bcryptjs.compare(password, user.password);
+        if (!validateUser) {
+          res.status(400).send("User email or password is incorrect");
+        } else {
+          const payload = {
+            userId: user._id,
+            email: user.email,
+          };
+          const JWT_SECRET_KEY =
+            process.env.JWT_SECRET_KEY || "THIS_IS_A_JWT_SECRET_KEY";
+
+          jwt.sign(
+            payload,
+            JWT_SECRET_KEY,
+            { expiresIn: 84600 },
+            async (err, token) => {
+              await Users.updateOne(
+                { _id: user._id },
+                {
+                  $set: { token },
+                }
+              );
+              user.save();
+              return res.status(200).json({
+                user: {
+                  id: user._id,
+                  email: user.email,
+                  fullName: user.fullName,
+                },
+                token: token,
+              });
+            }
+          );
+        }
+      }
+    }
+  } catch (error) {
+    console.log(error, "Error");
+  }
+};
+
+export {register,login}
--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,87 +1,9 @@
 import {Router} from "express";
-import Users from "../models/users.model"
-import bcryptjs from "bcryptjs";
-import jwt from "jsonwebtoken"
-// import cors from "cors"
+import { login,register } from "../controllers/auth.controllers";
 
 const router = Router();
 
-router.post("/register", async(req, res,next) => {
-   try {
-     const { fullName, email, password } = req.body;
-
-     if (!fullName || !email || !password) {
-       res.status(400).send("Please fill all required fields");
-     } else {
-       const isAlreadyExist = await Users.findOne({ email });
-       if (isAlreadyExist) {
-         res.status(400).send("User already exists");
-       } else {
-         const newUser = new Users({ fullName, email });
-         bcryptjs.hash(password, 10, (err, hashedPassword) => {
-           newUser.set("password", hashedPassword);
-           newUser.save();
-           next();
-         });
-         return res.status(200).send("User registered successfully");
-       }
-     }
-   } catch (error) {
-     console.log(error, "Error");
-   }
-});
-router.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    if (!email || !password) {
-      res.status(400).send("Please fill all required fields");
-    } else {
-      const user = await Users.findOne({ email });
-      if (!user) {
-        res.status(400).send("User email or password is incorrect");
-      } else {
-        const validateUser = await bcryptjs.compare(password, user.password);
-        if (!validateUser) {
-          res.status(400).send("User email or password is incorrect");
-        } else {
-          const payload = {
-            userId: user._id,
-            email: user.email,
-          };
-          const JWT_SECRET_KEY =
-            process.env.JWT_SECRET_KEY || "THIS_IS_A_JWT_SECRET_KEY";
-
-          jwt.sign(
-            payload,
-            JWT_SECRET_KEY,
-            { expiresIn: 84600 },
-            async (err, token) => {
-              await Users.updateOne(
-                { _id: user._id },
-                {
-                  $set: { token },
-                }
-              );
-              user.save();
-              return res
-                .status(200)
-                .json({
-                  user: {
-                    id: user._id,
-                    email: user.email,
-                    fullName: user.fullName,
-                  },
-                  token: token,
-                });
-            }
-          );
-        }
-      }
-    }
-  } catch (error) {
-    console.log(error, "Error");
-  }
-});
+router.post("/register",register);
+router.post("/login",login);
 
 export default router;
